b07e59997486908c01eb25ed19bd6ccdaf98f879
Marco <4high20@gmail.com>
2020-06-06 07:40:59 +0100

    Second commit

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
 })
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
